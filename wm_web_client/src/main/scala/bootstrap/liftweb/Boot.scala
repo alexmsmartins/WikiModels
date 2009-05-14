4c9761f0519e0a834d572e8f1ad63f7c78e0fbe5
Goncalo Neto <gonneto@gmail.com>
2009-05-14 19:59:13 +0100

    Added the prototyte and the wm_web_client src

--- wm_web_client/src/main/scala/bootstrap/liftweb/Boot.scala
+++ wm_web_client/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -4,20 +4,32 @@ import _root_.net.liftweb.util._
 import _root_.net.liftweb.http._
 import _root_.net.liftweb.sitemap._
 import _root_.net.liftweb.sitemap.Loc._
+import com.project.model._
 import Helpers._
 
 /**
   * A class that's instantiated early and run.  It allows the application
   * to modify lift's environment
   */
+
+object MenuInfo {
+    import Loc._
+    
+    def menu: List[Menu] =  Menu(Loc("home", List("index"), " Home ", Hidden)) ::
+    Menu(Loc("logout", List("menu","logout"), " Logout ", Hidden)) ::
+    Menu(Loc("profile", List("menu","profile"), " Profile ", Hidden)) ::
+    Menu(Loc("recover", List("menu","recover"), " Recover Password ")) ::
+    Menu(Loc("create", List("menu","create"), " Register ")) :: Nil
+
+}
+
 class Boot {
   def boot {
     // where to search snippet
-    LiftRules.addToPackages("pt.cnc.wikimodels")
+    LiftRules.addToPackages("com.project")
 
     // Build SiteMap
-    val entries = Menu(Loc("Home", List("index"), "Home")) :: Nil
-    LiftRules.setSiteMap(SiteMap(entries:_*))
+    LiftRules.setSiteMap(SiteMap(MenuInfo.menu:_*))
   }
 }
 
