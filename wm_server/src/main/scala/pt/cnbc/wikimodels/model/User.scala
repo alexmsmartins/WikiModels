3b1dcd6699d6475f07f1fef7bb5bafaa60445ea7
Alexandre Martins <alexmsmartins@gmail.com>
2009-05-26 13:25:57 +0100

    UseCase002 - Login  * configured log4j to be able to use httpcommons easily in the wm_rest_api  * implemented a request for imformation about a user with return of a stub of a user (without authetication for now)

---
+++ wm_server/src/main/scala/pt/cnbc/wikimodels/model/User.scala
@@ -0,0 +1,12 @@
+/*
+ * User.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package pt.cnbc.wikimodels.model
+
+class User(userName:String , password:String ) {
+
+}
