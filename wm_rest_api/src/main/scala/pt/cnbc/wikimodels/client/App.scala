658c67ef45eda2ec4862df6e7d85f22438213a2b
Alexandre Martins <alexmsmartins@gmail.com>
2009-05-26 23:04:25 +0100

    UseCase02 -Login  * header values can be passed from client to server Header

--- wm_rest_api/src/main/scala/pt/cnbc/wikimodels/client/App.scala
+++ wm_rest_api/src/main/scala/pt/cnbc/wikimodels/client/App.scala
@@ -1,12 +1,23 @@
-package pt.cnc.wikimodels.client
+package pt.cnbc.wikimodels.client
 
 
-
+import pt.cnbc.wikimodels.rest.client.RestfulAccess
+import org.apache.log4j._
 
 /**
  * Hello world!
  *
  */
-object App extends Application {
-  println( "Hello World!" )
+object App {
+    def main(args:Array[String]) = {
+        var ra = new RestfulAccess("alex", "alexp")
+        println( "======================" )
+        ra.sendRequest("GET","http://localhost:8080/wm_server-1.0-SNAPSHOT/resources/user/alex")
+        println( "======================" )
+        var log  = Logger.getLogger(App.getClass)
+
+        println( "======================" )
+        println( "= Hello World!       =" )
+        println( "======================" )
+    }
 }
