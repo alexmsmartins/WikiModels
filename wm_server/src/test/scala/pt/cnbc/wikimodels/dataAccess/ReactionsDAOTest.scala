98c15712b157152a568eb55c3092da77fb327f13
Alexandre Martins <alexmsmartins@gmail.com>
2010-06-01 04:17:29 +0100

    Solved a bug by whici the sub-entities in sbml weren't saved to the knowledgebase Changes:  - continued ReactionDAOTest  - changed wm_server/pom.xml to use antrun to copy sbml.owl and use it in ReactionDAOTest  - removed modelMetaIdExists(sbmlEntity:Element) and replaced every call by modelMetaIdExists(sbmlEntity:Element, model:Model)  - removed metaIdExists(sbmlEntity:Element) and replaced every call by metaIdExists(sbmlEntity:Element, model:Model)  - removed generateNewMetaIdFromString(sbmlEntity:Element) and replaced every call by generateNewMetaIdFromString(sbmlEntity:Element, model:Model)  - removed useless try{..} catch {...} blocks in createXXX(xxx:XXX, model:Model).  - there is still a NullPointerException in ReactionDAOTest

---
+++ wm_server/src/test/scala/pt/cnbc/wikimodels/dataAccess/ReactionsDAOTest.scala
@@ -0,0 +1,28 @@
+package pt.cnbc.wikimodels.dataAccess
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alex
+ * Date: 26/Mai/2010
+ * Time: 12:24:52
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.junit._
+import Assert._
+
+class ReactionsDAOTest {
+  @Before
+    
+  def setUp: Unit = {
+    }
+
+  @After
+  def tearDown: Unit = {
+    }
+
+  @Test
+  def example = {
+    }
+
+}
