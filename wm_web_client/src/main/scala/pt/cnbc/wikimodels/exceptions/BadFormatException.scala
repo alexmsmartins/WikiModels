8445d1e6ebf10c11ff3e87824c6411ba9292a1bf
Goncalo Neto <gonneto@gmail.com>
2010-06-02 14:00:31 +0100

    Prepared the Creation of the model for exceptions.

---
+++ wm_web_client/src/main/scala/pt/cnbc/wikimodels/exceptions/BadFormatException.scala
@@ -0,0 +1,14 @@
+/*
+ * BadFormatException.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package pt.cnbc.wikimodels.exceptions
+
+
+
+class BadFormatException(str:String) extends Exception(str){
+}
+
