a051701c4a888ff094cc142571c1b1a6f9574902
Alexandre Martins <alexmsmartins@gmail.com>
2011-05-30 01:33:30 +0100

    Templates for key file types in the project were added. This should be used to automate file creation a bit.

---
+++ templates/TemplateTest.scala
@@ -0,0 +1,21 @@
+package pt.cnbc.wikimodels.package
+
+import org.junit._
+import Assert._
+
+class ExampleTest {
+
+  @Before
+  def setUp: Unit = {
+  }
+
+  @After
+  def tearDown: Unit = {
+  }
+
+  @Test
+  def example = {
+  }
+
+}
+
