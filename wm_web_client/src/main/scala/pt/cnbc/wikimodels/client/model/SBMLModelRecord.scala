f9fe8da6180ed50c506d61bed538411c95249e3e
Alexandre Martins <alexmsmartins@gmail.com>
2011-07-13 02:28:51 +0100

    Updated to scala 2.9.0 amd Lift 2.4-M2. Changed some Lift-Record code.

--- wm_web_client/src/main/scala/pt/cnbc/wikimodels/client/model/SBMLModelRecord.scala
+++ wm_web_client/src/main/scala/pt/cnbc/wikimodels/client/model/SBMLModelRecord.scala
@@ -3,6 +3,7 @@ package pt.cnbc.wikimodels.client.model
 import net.liftweb.record._
 import xml.NodeSeq
 import net.liftweb.http.StringField
+import java.net.URI
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,30 +12,43 @@ import net.liftweb.http.StringField
  * Time: 17:37
  * To change this template use File | Settings | File Templates.
  */
-/*class SBMLModelRecord extends KeyedRecord[SBMLModelRecord]{
+/*class SBMLModelRecord extends KeyedRecord[SBMLModelRecord, URI]{
   def meta = SBMLModelRecord
 
-  object metaId extends StringField(this, "defaultMetaId")
+  object metaId extends KeyField[StringField, SBMLModelRecord]
+  object metaId2 extends StringField(default = "defaultMetaId")
   object id extends StringField(this, "defaultId")
   object name extends StringField(this, "defaultName")
   object description extends StringField(this, "")
 
 
-  def primaryKey: KeyField[StringField, SBMLModelRecord] = metaId
 
 //  can be created
   def create( ) = {
     pt.cnbc.wikimodels.snippet.User.restfulConnection.postRequest("/model/"+ modelId, <model></model>)
   }
 //  can be validated with validate
-  def validate = {
-    validate
-  }
 //  can be presented as XHtml, Json, or as a Form.
 
 
-  def toXHTML():NodeSeq
-  def toForm():NodeSeq
+  def toXHTML():NodeSeq = null
+  def toForm():NodeSeq = <form method="post" action="/models/createEdit">
+    <div id="tab-1">
+          <h4><span id="required_field">     * required fields</span></h4><br />
+          <ul class="treeview-gray" id="model_tree">
+              <li><span><h3>Model ID: <span id="required_field">*</span><div id="messages"></div></h3><br /></span></li>
+              <li><h3>Name of the model:
+                      <input onkeypress="liftUtils.lift_blurIfReturn(event)" size="40" type="text" value="" id="name_model" maxlength="1000" onblur="liftAjax.lift_ajaxHandler('F425848098740IJ2PSK=' + encodeURIComponent(this.value), null, null, null)" /></h3><br /></li>
+              <li><span><h3>Description of the model:</h3></span>
+                  <br />
+                  <ul>
+                      <li><span><textarea id="descriptionArea" name="F425848098731ZADFZH" maxlength="20000"></textarea></span><br /></li>
+                  </ul>
+              </li>
+          </ul>
+      </div>
+    </form>
+
 //  will contain fields which can be listed with allFields.
 
 //  can be created directly from a Request containing params with names that match the fields on a Record ( see fromReq ).
@@ -43,11 +57,11 @@ import net.liftweb.http.StringField
 
 object SBMLModelRecord extends SBMLModelRecord with MetaRecord[SBMLModelRecord]{
   protected val rootClass: Class[Any] = _
-}
+}*/
 
 
 
-sealed trait ManipModelState{ stateName:String){
+sealed class ManipModelState( stateName:String){
   case object EnterPage extends ManipModelState("EnterPage")
   case object Create extends ManipModelState("Create")
   case object CreateWithErrors extends ManipModelState("CreateWithErrors")
@@ -56,4 +70,4 @@ sealed trait ManipModelState{ stateName:String){
   case object Visualize extends ManipModelState("Visualize")
   case object GoToBrowseModelsPage extends ManipModelState("GoToBrowseModelsPage")
   case class AnotherState(_state:String) extends ManipModelState(_state)
-}*/
+}
