9ea211b7e9d29a89ab5947f9baa2395bbbd98b7b
Alexandre Martins <alexmsmartins@gmail.com>
2012-02-02 15:32:35 +0000

    XSLT transform changes to handle webkit's bugs. Didn't work.

--- wm_web_mathml/src/main/webapp/js/xsltTransformer.js
+++ wm_web_mathml/src/main/webapp/js/xsltTransformer.js
@@ -22,7 +22,7 @@ function loadXMLDoc(dname)
     }
     xhttp.open("GET",dname,false);
     xhttp.send("");
-    return xhttp.responseXML;
+    return xhttp.responseXML.documentElement;
 }
 function displayResult()
 {
@@ -51,13 +51,64 @@ function displayResult()
     // code for Mozilla, Firefox, Opera, etc.
     else if (document.implementation && document.implementation.createDocument)
     {
-        var xsltProcessor=new XSLTProcessor();
-        xsltProcessor.importStylesheet(xsl);
-        var resultDocument = xsltProcessor.transformToFragment(xml,document);
         var serializer = new XMLSerializer();
-        alert(serializer.serializeToString(resultDocument) )
+        //alert(xsl);
+        //alert(serializer.serializeToString(xsl));
+        //var xsltProcessor=new XSLTProcessor();
+        //alert(xsltProcessor);
+
+
+        //xsltProcessor.importStylesheet(xsl);
+
+        xsltProcessor = getProcessor(xsl); //fixes webkit's issues
+        //alert(xsltProcessor);
+        var resultDocument = xsltProcessor.transformToFragment(xml,document);
+
+        //if(resultDocument == null)
+            //alert("resutlDocuent is null!!!!")
+        //alert(resultDocument);
+        //alert(serializer.serializeToString(resultDocument) );
 
         var div = document.getElementById("formula");
         div.replaceChild(resultDocument, div.firstChild );
     }
+}
+
+
+//funtion taken from http://www.sitepoint.com/forums/showthread.php?769873-transformToFragment-on-webkit-browsers
+//they
+
+function fixWebKitStylesheet(doc)
+{
+    // ensure output method = html:
+    var node = doc.selectSingleNode("/xsl:stylesheet/xsl:output");
+    if (node && node.getAttribute("method")=="xml")
+        node.setAttribute("method", "html");
+
+    // gut out document node template to deal with Webkit bug
+    // Google: webkit bug 28744 "root one"
+    if ((node=doc.selectSingleNode("/xsl:stylesheet/xsl:template[@match='/']")))
+    {
+        var child;
+        while ((child=node.lastChild))
+            node.removeChild(child);
+        child = node.appendChild(doc.createElement("xsl:apply-templates"));
+        child.setAttribute("select", "*");
+    }
+}
+
+function getProcessor(doc)
+{
+    // detect WebKit implementation (incomplete example pulled out
+    // of other code that handles cross-browser XMLDOM differences):
+    var d = document.implementation.createDocument("","",null);
+    var isWebKit = !(d.load);
+
+    var proc = new XSLTProcessor();
+    if (isWebKit)
+        fixWebKitStylesheet(doc);
+
+    proc.importStylesheet(doc);
+
+    return proc;
 }
