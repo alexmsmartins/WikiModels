f72179674b2e9db3db072309fec9e5abe1db0f77
Alexandre Martins <alexmsmartins@gmail.com>
2013-03-13 15:47:09 +0000

    Glassfish decompression and begin of installation are possible in reproducible_env.sh Some refactoring also occured.

--- reproducible_env.sh
+++ reproducible_env.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# This file is supposed to create the entire working environment for WikiModels from scratch
+# This file is supposed to create the entire development environment for WikiModels from scratch
 # This includes removing the
 
 DOWNLOAD_DIR=reproducible_env_downloads
+GLASSFISH_JAR=glassfish-installer-v2.1.1-b31g-linux.jar
 
 #The  shell  shall write a message to standard error when it tries to expand a variable that is not set and immediately
 # exit.
@@ -16,12 +17,25 @@ echo "PWD="$PWD
 set -e
 
 echo "Check necessary programs"
-hash mvn 2>/dev/null || { echo >&2 "I require maven but it's not installed.  Aborting."; exit 1; }
-hash java 2>/dev/null || { echo >&2 "I require java but it's not installed.  Aborting."; exit 1; }
-hash javac 2>/dev/null || { echo >&2 "I require javac but it's not installed.  Aborting."; exit 1; }
-hash ant 2>/dev/null || { echo >&2 "I require ant but it's not installed.  Aborting."; exit 1; }
-hash wget 2>/dev/null || { echo >&2 "I require wget but it's not installed.  Aborting."; exit 1; }
-hash md5sum 2>/dev/null || { echo >&2 "I require md5sum but it's not installed. Aborting."; exit 1; }
+
+echo "before declaring function"
+
+check_command_presence() 
+{
+  echo "Checking for the presence of $1 in PATH"
+  hash $1 2>/dev/null || { echo >&2 "I require $1 but it's not installed.  Aborting."; exit 1; }
+}
+
+check_command_presence mvn
+check_command_presence java
+check_command_presence javac
+check_command_presence ant
+check_command_presence wget
+check_command_presence md5sum
+check_command_presence sudo     
+
+#Prompt user for the sudo password at the beginning of the script so it does not have to be asked later 
+sudo -v
 
 ############# install glassfish #############
 #rm -rf ~/glassfish
@@ -31,20 +45,48 @@ else
     mkdir ./$DOWNLOAD_DIR
 fi
 
-if [ -f ./$DOWNLOAD_DIR/glassfish-installer-v2.1.1-b31g-linux.jar ]; then
+if [ -f ./$DOWNLOAD_DIR/$GLASSFISH_JAR ]; then
     echo "Glassfis 2.1.1 was downloaded before"
 else
-    wget -P ./$DOWNLOAD_DIR http://download.java.net/javaee5/v2.1.1_branch/promoted/Linux/glassfish-installer-v2.1.1-b31g-linux.jar 
+    wget -P ./$DOWNLOAD_DIR http://download.java.net/javaee5/v2.1.1_branch/promoted/Linux/$GLASSFISH_JAR 
 fi
 
-md5sum -c ./reproducible_env_downloads/glassfish-installer-v2.1.1-b31g-linux.jar.md5
-ERR=$?
-echo "Error code: $ERR"
-if [ "$ERR" -eq 0 ]; then
+md5sum -c ./$DOWNLOAD_DIR/$GLASSFISH_JAR.md5
+RETVAL=$?
+if [ "$RETVAL" -eq 0 ]; then
 	echo "Glassfish 2.1.1 integrity checked!"
 else
 	echo >&2 "Glassfish 2.1.1 has an incorrect md5 hash. Aborting."
-	exit $ERR
+	exit $RETVAL
 fi
 
+# conmmands from http://glassfish.java.net/downloads/v2.1.1-final.html
+# To install and configure GlassFish you need to have JDK 5 or JDK 6 installed on your system. The configuration processing depends on Ant (1.6.5).  The bundle includes an Ant distribution that has been extended with tasks to facilitate developing Java EE 5 applications for the application server.
+
+#1) Download one of the bundles to disk, set JAVA_HOME to the JDK you have installed on your system.
+# Run:
+cd ./$DOWNLOAD_DIR
+java -Xmx256m -jar ./$GLASSFISH_JAR
+if [ -d ~/glassfish ]; then
+	echo "~/glassfish already exists. Aborting."
+	exit 1
+else
+	mv ./glassfish $HOME/	
+fi
+# This command will unbundle GlassFish and create a new directory structure rooted under a directory named 'glassfish'.
+cd $HOME/glassfish
+# If you are using a machine with an operating system that is a derivative of UNIX(tm), set the execute permission for the Ant binaries that are included with the GlassFish bundle.
+chmod -R +x lib/ant/bin
+lib/ant/bin/ant -f setup.xml 
+
+############# install postgres #############
+
+sudo apt-get -y install postgresql-9.1 
+
+############# install maven #############
+
+############# create glassfish realm #############
+
+############# setup databases #############
+sh ./setup-databases.sh 
 
